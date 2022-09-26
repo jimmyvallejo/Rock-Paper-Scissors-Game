bb5ee2765c7203cb92e1487b24799256f2250826
Jimmy <jrv1295@gmail.com>
2022-09-26 16:48:44 -0400

    Added rounds to game, and Win, lose, or draw message upon score reaching 5.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,11 @@
 
+// Declare initial score of player and computer//
+
 let playerScore = 0;
 let computerScore = 0;
 
 
-function computerPlay() {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-}
-
-let computerSelection = computerPlay();
-
-
-let playerSelection = prompt("Make your selection, Rock, paper or Scissors").toLowerCase();
+// Function designed for a single round of rock paper scissors, increments count +1 depending on outcome//
 
 function playRound(playerSelection, computerSelection){
     if ((playerSelection == "rock" && computerSelection == "scissors") ||
@@ -27,21 +21,48 @@ function playRound(playerSelection, computerSelection){
        }
 }
 
+// Function in order to get computer input randomly from an array//
+
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+let computerSelection = computerPlay();
 
 
+//Prompt in order to get user selection of (Rock, paper, or scissors)//
+
+let playerSelection = prompt("Make your selection, Rock, paper or Scissors").toLowerCase();
+
+//Function that plays a round of rock paper scissors until either CPU or user reaches 5//
 
 function game(){
-    if (playerScore < 5 && computerScore < 5) {
+    
+    while (playerScore < 5 && computerScore < 5){
+        
+        /*Variables redeclared inside while loop in order to get user 
+        input per round and random computer input*/
+        
         let playerSelection = prompt("Make your selection, Rock, paper or Scissors").toLowerCase();
-        playRound(playerSelection, computerSelection)
+        let computerSelection = computerPlay();
+        
+        console.log(playRound(playerSelection, computerSelection));
+        if (playerScore == 5 && computerScore == 5){
+            console.log("Draw! Refresh the page to try again.");
+        } else if (playerScore == 5 && computerScore < 5){
+            console.log("You Win! You Saved the world. Refresh to play again.")
+        } else if (playerScore < 5 && computerScore == 5) {
+            console.log("You lost :( Refresh the page to try again.")
+        }
     } 
 }
    
-game();
 
 
 
-console.log(playRound(playerSelection,computerSelection));
+
+console.log(game());
 
 
 
